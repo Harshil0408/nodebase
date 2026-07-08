16fa72aa56a1f131318929f6aa6e2bd4c4098ed4
Kanjariya Harshit <kanjariyaharshit@gmail.com>
2026-07-08 22:13:27 +0530

    01: setup

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return <Button variant={"outline"}>Click me</Button>;
+};
+
+export default page;
