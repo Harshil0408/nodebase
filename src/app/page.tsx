f7aaeea83c906de87160a1ab88c4b90d4c1ca7df
Kanjariya Harshit <kanjariyaharshit@gmail.com>
2026-07-11 21:09:39 +0530

    feat: add Inngest integration and workflow management
    - Added Inngest as a dependency for serverless function handling.
    - Created a new Workflow model in Prisma schema.
    - Updated the app router to include queries and mutations for workflows.
    - Implemented a new API route for Inngest functions.
    - Created a helloWorld function that responds with a message after a delay.
    - Updated the main page to display workflows and allow creation of new workflows.
    - Refactored login and registration forms to remove unused imports.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
 
-const page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data, isPending } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const data = await caller.getUsers();
-
-  console.log("data", data);
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    }),
+  );
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
       Protected Server Component
+      <pre>{JSON.stringify(data, null, 4)}</pre>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
     </div>
   );
 };
 
-export default page;
+export default Page;
